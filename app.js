681d0f65e18ab421e4dc8e8a8f4436bcbb3d8242
Srikar Maddala <maddalasrikar19@gmail.com>
2023-06-24 02:08:34 +0530

    done basic working

--- app.js
+++ app.js
@@ -5,21 +5,32 @@ const app = express();
 
 app.set("view engine" , 'ejs');
 
+app.use(bodyParser.urlencoded({extended:true}));
+
+var items = [];
+
 app.get("/" , function(req, res){
     
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-    if (currentDay == 6 || currentDay ===0){
-        day = "weekend";
-        res.write("Weekend bhai");
-    }else{
-        day = "Weekday";
-    }
-    res.render("list , {kindofday : day}");
-});
+    var options = {
+        weekday: 'long',
+        day : 'numeric',
+        month : 'long'
+    };
 
+    var day = today.toLocaleDateString("en-US" , options);
+    res.render("list" , { kindofday : day , newlistitems: items }); // key value 
+});
+// need to have a views folder
 
 app.listen(3000, function(){
     console.log("server is running on port 3000")
+})
+
+app.post("/" , function(req , res){
+    var item = req.body.newitem;
+    items.push(item);
+    console.log(item);
+
+    res.redirect("/");
 })
