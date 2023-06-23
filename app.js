1a210a3ced1e2c91731e08f27677fb531ac4179b
Srikar Maddala <maddalasrikar19@gmail.com>
2023-06-24 01:14:08 +0530

    ejs added

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine" , 'ejs');
+
+app.get("/" , function(req, res){
+    
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+    if (currentDay == 6 || currentDay ===0){
+        day = "weekend";
+        res.write("Weekend bhai");
+    }else{
+        day = "Weekday";
+    }
+    res.render("list , {kindofday : day}");
+});
+
+
+app.listen(3000, function(){
+    console.log("server is running on port 3000")
+})
